aa1e8ade950a3455ebb5f73c2c8652f0d441fc71
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-06-17 16:20:50 +0300

    Added linear algebra project with basic set of functions.

---
+++ Assets/AngleTester.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleTester : MonoBehaviour
+{
+    private void Start() {
+        Debug.Log(V3.AngleBetweenVectors(transform.forward, transform.right));
+    }
+}
---
+++ Assets/V3.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class V3
+{
+
+    public static Vector3 Addition(Vector3 a, Vector3 b) {
+        return new Vector3(a.x + b.x, a.y + b .y, a.z + b.z);
+    }
+
+    public static Vector3 Subtraction(Vector3 a, Vector3 b) {
+        return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static float Magnitude(Vector3 vector) {
+       return Mathf.Pow((vector.x*vector.x + vector.y*vector.y + vector.z*vector.z), 2.0f);
+    }
+
+    public static Vector3 ScalarMultiplication(Vector3 vector, float scalar) {
+        return new Vector3(vector.x * scalar, vector.y * scalar, vector.z * scalar);
+    }
+
+    public static Vector3 Normalized(Vector3 vector) {
+        float magnitude = V3.Magnitude(vector);
+        return new Vector3(vector.x / magnitude, vector.y / magnitude, vector.z / magnitude);
+    }
+
+    public static float DotProduct(Vector3 a, Vector3 b) {
+        // can be calculated as:
+        // V3.Magnitude(a)*V3.Magnitude(b)*Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(a, b));
+        return (a.x*b.x + a.y*b.y + a.z*b.z);
+    }
+
+    // Dot products: a*b = |a||b|cosΘ, where Θ - angle between vectors, so angle:
+    // angle = acos(a*b/|a||b|), if a and b normalized: angle = acos(a*b)
+    public static float AngleBetweenVectors(Vector3 a, Vector3 b) {
+        // if vector3 normalized, we can remove magnitudes:
+        // return Mathf.Acos (DotProduct(a, b)/ (Magnitude(a)*V3.Magnitude(b)));
+        return Mathf.Rad2Deg * Mathf.Acos (DotProduct(a, b));
+    }
+}
